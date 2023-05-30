8c3479b3cc958c4afa48fb27a9e518543725bd13
Arthur Dias <a.dias@cyberdian.fr>
2023-05-30 23:06:15 +0200

    commit

---
+++ src/utils/database.ts
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_SSBCGB_0opdwubYYUPjJUkzmbaN3ROo",
+    authDomain: "leafy-4f506.firebaseapp.com",
+    databaseURL: "https://leafy-4f506-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "leafy-4f506",
+    storageBucket: "leafy-4f506.appspot.com",
+    messagingSenderId: "99751034639",
+    appId: "1:99751034639:web:d4d715f8d79380c3ca087a",
+    measurementId: "G-EERTEV8HEW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getFirestore(app);
+
+async function getCities(db) {
+    const citiesCol = collection(db, 'utilisateurs');
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map(doc => doc.data());
+    return cityList;
+}
+
+export default analytics;
