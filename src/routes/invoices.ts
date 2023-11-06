8ea7ee5be9e358d7cbd9dd46d0d359628c796208
Laure Bellanger <5022584-laurebll@users.noreply.gitlab.com>
2023-11-06 02:33:58 +0100

    fix: updated storage on increase

--- src/routes/invoices.ts
+++ src/routes/invoices.ts
@@ -5,6 +5,9 @@ import {
   getDocs,
   serverTimestamp,
   where,
+  getDoc,
+  doc,
+  updateDoc,
 } from "firebase/firestore";
 import { db } from "../utils/database";
 import { Router } from "express";
@@ -49,9 +52,38 @@ app.get("/", auth, async (req: any, res) => {
     const invoices = (await getDocs(q)).docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
+      date: doc.data().date.toDate(),
     }));
 
     res.status(200).json({ invoices });
+  } catch (error: any) {
+    return res.status(error.statusCode).json({ error });
+  }
+});
+
+app.post("/", auth, async (req: any, res) => {
+  try {
+    const { city, zip, country, address } = req.body;
+
+    const addr = {
+      city,
+      zip,
+      country,
+      address,
+    };
+
+    const userId = req.auth.userId;
+    await addInvoice({ address: addr, userId });
+
+    const docRef: any = doc(db, "utilisateurs", req.auth.userId);
+    const user: any = (await getDoc(docRef)).data();
+
+    const dataEdited: any = {
+      storage: { used: user.storage.used, max: user.storage.max + 21474836480 },
+    };
+    await updateDoc(docRef, dataEdited);
+
+    res.status(201).json("Successfully payed");
   } catch (error: any) {
     return res.status(error.statusCode).json({ error });
   }
